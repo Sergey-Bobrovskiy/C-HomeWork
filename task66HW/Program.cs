b42483a99da5d78bc2f927e81d7cce11b760dd8b
Bobrovskij <79031844536@mail.ru>
2023-07-21 18:05:03 +0300

    Homework9

---
+++ task66HW/Program.cs
@@ -0,0 +1,15 @@
+﻿System.Console.WriteLine("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());  
+System.Console.WriteLine("Введите  число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Result, S = {SumNumbers(M, N)}");
+Console.ReadLine();
+ 
+static int SumNumbers(int M, int N)
+{
+    if (M == 0) return (N * (N + 1)) / 2;            // Если M равно нулю
+    else if (N == 0) return (M * (M + 1)) / 2;       // Если N равно нулю
+    else if (M == N) return M;                       // Если M=N
+    else if (M < N) return N + SumNumbers(M, N - 1); // Если M<N
+    else return N + SumNumbers(M, N + 1);            // Если M>N
+}
